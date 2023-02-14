9f44a344afacb5537bfd3557147689b62e24b825
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-02-14 17:46:33 +0300

    v1.0

---
+++ firstexample/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["firstexample/firstexample/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ firstexample/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"firstexample/firstexample/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
